042bcb32452d8fd6b2ed80a5071834f0b6dfbb02
RegolithIII <85413146+RegolithIII@users.noreply.github.com>
2023-05-28 14:11:14 +0200

    Update code.py

--- code.py
+++ code.py
@@ -12,6 +12,7 @@ from adafruit_hid.consumer_control import ConsumerControl
 from adafruit_hid.consumer_control_code import ConsumerControlCode
 from analogio import AnalogIn
 
+# ---------------------------------------------------------------
 # Set up Consumer Control - Control Codes can be found here: https://docs.circuitpython.org/projects/hid/en/latest/_modules/adafruit_hid/consumer_control_code.html#ConsumerControlCode
 cc = ConsumerControl(usb_hid.devices)
 
@@ -21,19 +22,18 @@ keyboard = Keyboard(usb_hid.devices)
 # Set up keyboard to write strings from macro
 write_text = KeyboardLayoutUS(keyboard)
 
-
+# ---------------------------------------------------------------
+# LCD declarations
 i2c = busio.I2C(scl=board.GP1, sda=board.GP0)
-address = 0x3f
 
+address = 0x3f # Put the address given by ic2_scan here
 
 i2c = i2c_pcf8574_interface.I2CPCF8574Interface(i2c, address)
-
 display = lcd.LCD(i2c, num_rows=2, num_cols=16)
-
 display.set_backlight(True)
-
 display.set_display_enabled(True)
 
+# Starting animation here, purely aesthetics
 display.clear()
 display.set_cursor_pos(0,0)
 display.print("Starting.")
@@ -43,21 +43,56 @@ time.sleep(1)
 display.print(".")
 time.sleep(1)
 
-#---------------------------------------------------------------
-# Run the main loop to generate a counting display.
-prev = digitalio.DigitalInOut(board.GP13)
-prev.switch_to_input(pull=digitalio.Pull.DOWN)
+# ---------------------------------------------------------------
+# Buttons declarations
+btn1 = digitalio.DigitalInOut(board.GP15)
+btn1.switch_to_input(pull=digitalio.Pull.DOWN)
 
-play = digitalio.DigitalInOut(board.GP14)
-play.switch_to_input(pull=digitalio.Pull.DOWN)
+btn2 = digitalio.DigitalInOut(board.GP14)
+btn2.switch_to_input(pull=digitalio.Pull.DOWN)
 
-next = digitalio.DigitalInOut(board.GP15)
-next.switch_to_input(pull=digitalio.Pull.DOWN)
+btn3 = digitalio.DigitalInOut(board.GP13)
+btn3.switch_to_input(pull=digitalio.Pull.DOWN)
 
 btn4 = digitalio.DigitalInOut(board.GP12)
 btn4.switch_to_input(pull=digitalio.Pull.DOWN)
-# Potentiometer related stuff
 
+# What buttons do :
+# Keycode class defines USB HID keycodes to send using Keyboard.  
+def btn1Action():
+    cc.send(ConsumerControlCode.PLAY_PAUSE)
+    display.clear()
+    getVolume()
+    display.set_cursor_pos(1,0)
+    display.print("Play/Pause")
+    time.sleep(0.5)
+    
+def btn2Action():
+    cc.send(ConsumerControlCode.SCAN_NEXT_TRACK)
+    display.clear()
+    getVolume()
+    display.set_cursor_pos(1,0)
+    display.print("Next")
+    time.sleep(0.5)
+    
+def btn3Action():
+    display.clear()
+    getVolume()
+    display.set_cursor_pos(1,0)
+    display.print("Open Brave")
+    keyboard.send(Keycode.GUI, Keycode.TWO)
+    time.sleep(0.4)
+    
+def btn4Action():
+    display.clear()
+    getVolume()
+    display.set_cursor_pos(1,0)
+    display.print("Open Discord")
+    keyboard.send(Keycode.GUI, Keycode.FOUR)
+    time.sleep(0.4)
+
+# ---------------------------------------------------------------    
+# Potentiometer related stuff
 READ_TIME = 0.001
 
 def map_function(x, in_min, in_max, out_min, out_max):
@@ -77,9 +112,55 @@ for i in range(32):
 current_volume = 0
 last_position = 0
 
+# ---------------------------------------------------------------
 # Custom characters
+# Creator : https://educ8s.tv/tools/lcd-character-creator/
 volume_char = (0x02,0x09,0x05,0x15,0x15,0x05,0x09,0x02)
 display.create_char(0,volume_char)
+
+discord1_char = (0x00,0x0C,0x1E,0x1F,0x17,0x1F,0x1F,0x06)
+display.create_char(1,discord1_char)
+discord2_char = (0x00,0x06,0x0F,0x1F,0x1D,0x1F,0x1F,0x0C)
+display.create_char(2,discord2_char)
+
+brave1_char = (0x03,0x04,0x0B,0x09,0x09,0x04,0x04,0x03)
+display.create_char(3,brave1_char)
+brave2_char = (0x10,0x08,0x14,0x04,0x04,0x08,0x08,0x10)
+display.create_char(4,brave2_char)
+
+play_char = (0x10,0x18,0x1C,0x1E,0x1E,0x1C,0x18,0x10)
+display.create_char(5,play_char)
+pause_char = (0x1B,0x1B,0x1B,0x1B,0x1B,0x1B,0x1B,0x1B)
+display.create_char(6,pause_char)
+
+def DiscordLogo():
+    display.set_cursor_pos(0,14)
+    display.write(2)
+    display.set_cursor_pos(0,15)
+    display.write(1)
+    display.home()
+
+def BraveLogo():
+    display.set_cursor_pos(0,10)
+    display.write(3)
+    display.set_cursor_pos(0,11)
+    display.write(4)
+    display.home()
+    
+def playPause():
+    display.set_cursor_pos(0,0)
+    display.write(5)
+    display.set_cursor_pos(0,1)
+    display.write(6)
+    display.home()
+    
+def nextTrack():
+    display.set_cursor_pos(0,4)
+    display.write(5)
+    display.set_cursor_pos(0,5)
+    display.write(5)
+    display.home()
+    
 
 def getVolume():
     display.clear()
@@ -87,20 +168,17 @@ def getVolume():
     display.write(0)
     display.print(str(current_volume))
     display.home()
+    DiscordLogo()
+    BraveLogo()
+    playPause()
+    nextTrack()
     
+# ---------------------------------------------------------------
+# Loop
 while True:
-
-    # Keycode class defines USB HID keycodes to send using Keyboard.  
-#    if btn1.value:
-#        display.clear()
-#        display.print("Open VLC")
-#        keyboard.send(Keycode.GUI)
-#        time.sleep(0.4)
-#
-    
     time.sleep(0.1)
     
-    #Volume Control
+#Volume Control
     if time.monotonic() - last_read > READ_TIME:
         position = int(map_function(potentiometer.value, 200, 65520, 0, 100))
 
@@ -129,31 +207,17 @@ while True:
     # handle time.monotonic() overflow
     if time.monotonic() < last_read:
         last_read = time.monotonic
-        
-    if play.value:
-        cc.send(ConsumerControlCode.PLAY_PAUSE)
-        display.clear()
-        getVolume()
-        display.print("Play/Pause")
-        time.sleep(0.5)
 
-    if next.value:
-        cc.send(ConsumerControlCode.SCAN_NEXT_TRACK)
-        display.clear()
-        getVolume()
-        display.print("Next")
-        time.sleep(0.5)
+# ---------------------------------------------------------------
+# Button press listening
+    if btn1.value:
+        btn1Action()
 
-    if prev.value:
-        cc.send(ConsumerControlCode.SCAN_PREVIOUS_TRACK)
-        display.clear()
-        getVolume()
-        display.print("Prev")
-        time.sleep(0.5)
+    if btn2.value:
+        btn2Action()
+
+    if btn3.value:
+        btn3Action()
       
     if btn4.value:
-        display.clear()
-        getVolume()
-        display.print("Open Discord")
-        keyboard.send(Keycode.GUI, Keycode.FOUR)
-        time.sleep(0.4)
+        btn4Action()
